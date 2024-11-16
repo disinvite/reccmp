508b2a7040371d5332dc36958cb3121d6cecc168
disinvite <disinvite@users.noreply.github.com>
2024-11-16 14:46:20 -0500

    Condense states for nameref-only markers. state enum refactor

--- reccmp/isledecomp/parser/parser.py
+++ reccmp/isledecomp/parser/parser.py
@@ -1,7 +1,7 @@
 # C++ file parser
 
 from typing import List, Iterable, Iterator, Optional
-from enum import Enum
+import enum
 from .util import (
     get_class_name,
     get_variable_name,
@@ -28,18 +28,22 @@ from .error import ParserAlert, ParserError
 from .tokenizer import TokenType, tokenize  # pylint: disable=unused-import
 
 
-class ReaderState(Enum):
-    SEARCH = 0
-    WANT_SIG = 1
-    IN_FUNC = 2
-    IN_TEMPLATE = 3
-    WANT_CURLY = 4
-    IN_GLOBAL = 5
-    IN_FUNC_GLOBAL = 6
-    IN_VTABLE = 7
-    IN_SYNTHETIC = 8
-    IN_LIBRARY = 9
-    DONE = 100
+class ReaderState(enum.Enum):
+    # Waiting for the next annotation marker
+    SEARCH = enum.auto()
+    # Read a function annotation, waiting for nameref comment or code tokens
+    FUNCTION_PENDING = enum.auto()
+    # Collecting code tokens to find start of function
+    FUNCTION_COLLECT = enum.auto()
+    # We read one of the nameref-only function annotations. Waiting for comment
+    FUNCTION_NAMEREF = enum.auto()
+    # Inside the function, waiting for ending curly brace.
+    IN_FUNC = enum.auto()
+    IN_GLOBAL = enum.auto()
+    IN_FUNC_GLOBAL = enum.auto()
+    IN_VTABLE = enum.auto()
+    #
+    DONE = enum.auto()
 
 
 class MarkerDict:
@@ -240,19 +244,13 @@ class DecompParser:
     def _function_marker(self, marker: DecompMarker):
         if self.fun_markers.insert(marker):
             self._syntax_warning(ParserError.DUPLICATE_MODULE)
-        self.state = ReaderState.WANT_SIG
+        self.state = ReaderState.FUNCTION_PENDING
 
     def _nameref_marker(self, marker: DecompMarker):
         """Functions explicitly referenced by name are set here"""
         if self.fun_markers.insert(marker):
             self._syntax_warning(ParserError.DUPLICATE_MODULE)
-
-        if marker.is_template():
-            self.state = ReaderState.IN_TEMPLATE
-        elif marker.is_synthetic():
-            self.state = ReaderState.IN_SYNTHETIC
-        else:
-            self.state = ReaderState.IN_LIBRARY
+        self.state = ReaderState.FUNCTION_NAMEREF
 
     def _function_done(self, lookup_by_name: bool = False, unexpected: bool = False):
         end_line = self.line_number
@@ -369,7 +367,7 @@ class DecompParser:
 
     def _handle_marker(self, marker: DecompMarker):
         # Cannot handle any markers between function sig and opening curly brace
-        if self.state == ReaderState.WANT_CURLY:
+        if self.state == ReaderState.FUNCTION_COLLECT:
             self._syntax_error(ParserError.UNEXPECTED_MARKER)
             return
 
@@ -392,7 +390,7 @@ class DecompParser:
         if marker.is_regular_function():
             if self.state in (
                 ReaderState.SEARCH,
-                ReaderState.WANT_SIG,
+                ReaderState.FUNCTION_PENDING,
             ):
                 # We will allow multiple offsets if we have just begun
                 # the code block, but not after we hit the curly brace.
@@ -400,20 +398,9 @@ class DecompParser:
             else:
                 self._syntax_error(ParserError.INCOMPATIBLE_MARKER)
 
-        elif marker.is_template():
-            if self.state in (ReaderState.SEARCH, ReaderState.IN_TEMPLATE):
-                self._nameref_marker(marker)
-            else:
-                self._syntax_error(ParserError.INCOMPATIBLE_MARKER)
-
-        elif marker.is_synthetic():
-            if self.state in (ReaderState.SEARCH, ReaderState.IN_SYNTHETIC):
-                self._nameref_marker(marker)
-            else:
-                self._syntax_error(ParserError.INCOMPATIBLE_MARKER)
-
-        elif marker.is_library():
-            if self.state in (ReaderState.SEARCH, ReaderState.IN_LIBRARY):
+        elif marker.is_explicit_byname():
+            top_marker = next(self.fun_markers.iter(), None)
+            if top_marker is None or top_marker.type == marker.type:
                 self._nameref_marker(marker)
             else:
                 self._syntax_error(ParserError.INCOMPATIBLE_MARKER)
@@ -517,10 +504,8 @@ class DecompParser:
                 return
 
             if self.state in (
-                ReaderState.WANT_SIG,
-                ReaderState.IN_SYNTHETIC,
-                ReaderState.IN_TEMPLATE,
-                ReaderState.IN_LIBRARY,
+                ReaderState.FUNCTION_PENDING,
+                ReaderState.FUNCTION_NAMEREF,
             ):
                 # Explicit nameref functions provide the function name
                 # on the next line (in a // comment)
@@ -537,10 +522,10 @@ class DecompParser:
         self.curly.read_token(token)
 
         # Don't read more comments
-        if self.state == ReaderState.WANT_SIG:
-            self.state = ReaderState.WANT_CURLY
+        if self.state == ReaderState.FUNCTION_PENDING:
+            self.state = ReaderState.FUNCTION_COLLECT
 
-        if self.state == ReaderState.WANT_CURLY:
+        if self.state == ReaderState.FUNCTION_COLLECT:
             if token[2] == ";":
                 self._syntax_error(ParserError.NO_IMPLEMENTATION)
             elif token[2] == "{":

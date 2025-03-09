92b3fe7dba5a66ae48d70370221d61920194731a
disinvite <disinvite@users.noreply.github.com>
2025-03-08 23:21:46 -0500

    Remove glob

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,11 +38,11 @@ def fixture_loader(
 ) -> Iterator[Callable[[str, str], Image | None]]:
     def loader(filename: str, hash_str: str) -> Image | None:
         if binfile_path is not None:
-            for file in binfile_path.glob(filename, case_sensitive=False):
+            file = binfile_path / filename
+            if file.exists():
                 if not check_hash(file, hash_str):
                     pytest.fail(reason="Did not match expected " + filename.upper())
 
-                # Use only the first match
                 return detect_image(file)
 
         not_found_reason = "No path to " + filename.upper()
@@ -60,7 +60,7 @@ def fixture_loader(
 def fixture_binfile(bin_loader) -> Iterator[PEImage]:
     """LEGO1.DLL: v1.1 English, September"""
     image = bin_loader(
-        "lego1.dll", "14645225bbe81212e9bc1919cd8a692b81b8622abb6561280d99b0fc4151ce17"
+        "LEGO1.DLL", "14645225bbe81212e9bc1919cd8a692b81b8622abb6561280d99b0fc4151ce17"
     )
     assert isinstance(image, PEImage)
     yield image
@@ -71,7 +71,7 @@ def fixture_skifree(bin_loader) -> Iterator[NEImage]:
     """SkiFree 1.0
     https://ski.ihoc.net/"""
     image = bin_loader(
-        "ski.exe", "0b97b99fcf34af5f5d624080417c79c7d36ae11351a7870ce6e0a476f03515c2"
+        "SKI.EXE", "0b97b99fcf34af5f5d624080417c79c7d36ae11351a7870ce6e0a476f03515c2"
     )
     assert isinstance(image, NEImage)
     yield image

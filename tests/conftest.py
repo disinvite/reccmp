d30f47aa23fb2b4a958e41a62daecf30c822798d
disinvite <disinvite@users.noreply.github.com>
2025-03-08 23:09:11 -0500

    conftest changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,58 +1,77 @@
 import hashlib
 from pathlib import Path
-from typing import Iterator
+from typing import Callable, Iterator
 import pytest
 
-from reccmp.isledecomp import NEImage, PEImage, detect_image
+from reccmp.isledecomp import Image, NEImage, PEImage, detect_image
 
 
 def pytest_addoption(parser):
     """Allow the option to run tests against sample binaries."""
-    parser.addoption("--lego1", action="store", help="Path to LEGO1.DLL")
-    parser.addoption("--ski", action="store", help="Path to SKI.EXE")
+    parser.addoption("--binfiles", action="store", help="Path to sample binary files.")
+    parser.addoption(
+        "--require-binfiles",
+        action="store_true",
+        help="Fail tests that depend on binary samples if we cannot load them.",
+    )
 
 
-# LEGO1.DLL: v1.1 English, September
-LEGO1_SHA256 = "14645225bbe81212e9bc1919cd8a692b81b8622abb6561280d99b0fc4151ce17"
+@pytest.fixture(name="binfile_path", scope="session")
+def fixture_binfile_path(pytestconfig) -> Iterator[Path | None]:
+    path = pytestconfig.getoption("--binfiles")
+    if path is not None:
+        yield Path(path).resolve()
+        return
 
-# SkiFree 1.0
-# https://ski.ihoc.net/
-SKI_SHA256 = "0b97b99fcf34af5f5d624080417c79c7d36ae11351a7870ce6e0a476f03515c2"
+    yield None
+
+
+def check_hash(path: Path, hash_str: str) -> bool:
+    with path.open("rb") as f:
+        digest = hashlib.sha256(f.read()).hexdigest()
+        return digest == hash_str
+
+
+@pytest.fixture(name="bin_loader", scope="session")
+def fixture_loader(
+    pytestconfig, binfile_path
+) -> Iterator[Callable[[str, str], Image | None]]:
+    def loader(filename: str, hash_str: str) -> Image | None:
+        if binfile_path is not None:
+            for file in binfile_path.glob(filename, case_sensitive=False):
+                if not check_hash(file, hash_str):
+                    pytest.fail(reason="Did not match expected " + filename.upper())
+
+                # Use only the first match
+                return detect_image(file)
+
+        not_found_reason = "No path to " + filename.upper()
+        if pytestconfig.getoption("--require-binfiles"):
+            pytest.fail(reason=not_found_reason)
+
+        pytest.skip(allow_module_level=True, reason=not_found_reason)
+
+        return None
+
+    yield loader
 
 
 @pytest.fixture(name="binfile", scope="session")
-def fixture_binfile(pytestconfig) -> Iterator[PEImage]:
-    filename = pytestconfig.getoption("--lego1")
-
-    # Skip this if we have not provided the path to LEGO1.dll.
-    if filename is None:
-        pytest.skip(allow_module_level=True, reason="No path to LEGO1")
-
-    filename = Path(filename)
-    with filename.open("rb") as f:
-        digest = hashlib.sha256(f.read()).hexdigest()
-        if digest != LEGO1_SHA256:
-            pytest.fail(reason="Did not match expected LEGO1.DLL")
-
-    image = detect_image(filename)
+def fixture_binfile(bin_loader) -> Iterator[PEImage]:
+    """LEGO1.DLL: v1.1 English, September"""
+    image = bin_loader(
+        "lego1.dll", "14645225bbe81212e9bc1919cd8a692b81b8622abb6561280d99b0fc4151ce17"
+    )
     assert isinstance(image, PEImage)
     yield image
 
 
 @pytest.fixture(name="skifree", scope="session")
-def fixture_skifree(pytestconfig) -> Iterator[NEImage]:
-    filename = pytestconfig.getoption("--ski")
-
-    # Skip this if we have not provided the path to SKI.EXE.
-    if filename is None:
-        pytest.skip(allow_module_level=True, reason="No path to SKI")
-
-    filename = Path(filename)
-    with filename.open("rb") as f:
-        digest = hashlib.sha256(f.read()).hexdigest()
-        if digest != SKI_SHA256:
-            pytest.fail(reason="Did not match expected SKI.EXE")
-
-    image = detect_image(filename)
+def fixture_skifree(bin_loader) -> Iterator[NEImage]:
+    """SkiFree 1.0
+    https://ski.ihoc.net/"""
+    image = bin_loader(
+        "ski.exe", "0b97b99fcf34af5f5d624080417c79c7d36ae11351a7870ce6e0a476f03515c2"
+    )
     assert isinstance(image, NEImage)
     yield image

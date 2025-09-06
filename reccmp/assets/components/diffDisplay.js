d310664f3e71ed7f02e68dd9e40e71b319f98f59
disinvite <disinvite@users.noreply.github.com>
2025-09-05 23:57:17 -0400

    Move relevant util functions into component file

--- reccmp/assets/components/diffDisplay.js
+++ reccmp/assets/components/diffDisplay.js
@@ -1,6 +1,70 @@
-import { formatAsm, getDataByAddr, setBooleanAttribute } from '../globals';
+import { getDataByAddr } from '../globals';
 
 // reccmp-pack-begin
+
+function formatAsm(entries, _addrOption) {
+  const output = [];
+
+  const createTh = (text) => {
+    const th = document.createElement('th');
+    th.innerText = text;
+    return th;
+  };
+
+  const createTd = (text, className = '') => {
+    const td = document.createElement('td');
+    td.innerText = text;
+    td.className = className;
+    return td;
+  };
+
+  entries.forEach((obj) => {
+    // These won't all be present. You get "both" for an equal node
+    // and orig/recomp for a diff.
+    const { both = [], orig = [], recomp = [] } = obj;
+
+    output.push(
+      ...both.map(([addr, line, recompAddr]) => {
+        const tr = document.createElement('tr');
+        tr.appendChild(createTh(addr));
+        tr.appendChild(createTh(recompAddr));
+        tr.appendChild(createTd(line));
+        return tr;
+      }),
+    );
+
+    output.push(
+      ...orig.map(([addr, line]) => {
+        const tr = document.createElement('tr');
+        tr.appendChild(createTh(addr));
+        tr.appendChild(createTh(''));
+        tr.appendChild(createTd(`-${line}`, 'diffneg'));
+        return tr;
+      }),
+    );
+
+    output.push(
+      ...recomp.map(([addr, line]) => {
+        const tr = document.createElement('tr');
+        tr.appendChild(createTh(''));
+        tr.appendChild(createTh(addr));
+        tr.appendChild(createTd(`+${line}`, 'diffpos'));
+        return tr;
+      }),
+    );
+  });
+
+  return output;
+}
+
+// Helper for this set/remove attribute block
+function setBooleanAttribute(element, attribute, value) {
+  if (value) {
+    element.setAttribute(attribute, '');
+  } else {
+    element.removeAttribute(attribute);
+  }
+}
 
 class DiffDisplayOptions extends window.HTMLElement {
   static observedAttributes = ['data-option'];

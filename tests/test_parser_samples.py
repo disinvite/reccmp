40f199ca59a0877478c4e7c41933174c4981cc2f
disinvite <disinvite@users.noreply.github.com>
2026-07-28 14:19:16 -0400

    Delete sample .cpp files, embed in test_parser_samples.py

--- tests/test_parser_samples.py
+++ tests/test_parser_samples.py
@@ -1,17 +1,7 @@
-import os
-from typing import TextIO
+from textwrap import dedent
 import pytest
 from reccmp.parser import DecompParser
 from reccmp.parser.node import ParserSymbol
-
-SAMPLE_DIR = os.path.join(os.path.dirname(__file__), "samples")
-
-
-def sample_file(filename: str) -> TextIO:
-    """Wrapper for opening the samples from the directory that does not
-    depend on the cwd where we run the test"""
-    full_path = os.path.join(SAMPLE_DIR, filename)
-    return open(full_path, "r", encoding="utf-8")
 
 
 def code_blocks_are_sorted(blocks: list[ParserSymbol]) -> bool:
@@ -30,32 +20,71 @@ def fixture_parser():
 
 def test_sanity(parser):
     """Read a very basic file"""
-    with sample_file("basic_file.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // FUNCTION: TEST 0x1234
+        void function01()
+        {
+          // TODO
+        }
+
+        // FUNCTION: TEST 0x2345
+        void function02()
+        {
+          // TODO
+        }
+
+        // FUNCTION: TEST 0x3456
+        void function03()
+        {
+          // TODO
+        }
+    """))
 
     assert len(parser.functions) == 3
     assert code_blocks_are_sorted(parser.functions) is True
     # n.b. The parser returns line numbers as 1-based
     # Function begins on the first code line
-    assert parser.functions[0].line_number == 7
-    assert parser.functions[0].end_line == 10
+    assert parser.functions[0].line_number == 2
+    assert parser.functions[0].end_line == 5
 
 
 def test_oneline(parser):
     """(Assuming clang-format permits this) This sample has a function
     on a single line. This will test the end-of-function detection"""
-    with sample_file("oneline_function.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // FUNCTION: TEST 0x1234
+        void short_function() { static char* msg = "oneliner"; }
+
+        // FUNCTION: TEST 0x5555
+        void function_after_one_liner()
+        {
+          // This function comes after the previous that is on a single line.
+          // Do we report the offset for this one correctly?
+        }
+    """))
 
     assert len(parser.functions) == 2
-    assert parser.functions[0].line_number == 5
-    assert parser.functions[0].end_line == 5
+    assert parser.functions[0].line_number == 2
+    assert parser.functions[0].end_line == 2
 
 
 def test_missing_offset(parser):
     """What if the function doesn't have an offset comment?"""
-    with sample_file("missing_offset.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        #include <stdio.h>
+
+        int no_offset_comment()
+        {
+          static int dummy = 123;
+          return -1;
+        }
+
+        // FUNCTION: TEST 0xdeadbeef
+        void regular_ole_function()
+        {
+          printf("hi there");
+        }
+    """))
 
     # TODO: For now, the function without the offset will just be ignored.
     # Would be the same outcome if the comment was present but mangled and
@@ -67,38 +96,102 @@ def test_jumbled_case(parser):
     """The parser just reports what it sees. It is the responsibility of
     the downstream tools to do something about a jumbled file.
     Just verify that we are reading it correctly."""
-    with sample_file("out_of_order.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // FUNCTION: TEST 0x1001
+        void function_order01()
+        {
+            // TODO
+        }
+
+        // FUNCTION: TEST 0x1003
+        void function_order03()
+        {
+            // TODO
+        }
+
+        // FUNCTION: TEST 0x1002
+        void function_order02()
+        {
+            // TODO
+        }
+    """))
 
     assert len(parser.functions) == 3
     assert code_blocks_are_sorted(parser.functions) is False
 
 
 def test_bad_file(parser):
-    with sample_file("poorly_formatted.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // FUNCTION: TEST 0x1234
+        void curly_with_spaces()
+          {
+          static char* msg = "hello";
+          }
+
+        // FUNCTION: TEST 0x5555
+        void weird_closing_curly()
+        {
+          int x = 123; }
+
+        // FUNCTION: HELLO 0x5656
+        void bad_indenting() {
+          if (0)
+        {
+          int y = 5;
+        }}
+    """))
 
     assert len(parser.functions) == 3
 
 
 def test_indented(parser):
     """Offsets for functions inside of a class will probably be indented."""
-    with sample_file("basic_class.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // VTABLE: TEST 0x1001002
+        class TestClass {
+        public:
+          TestClass();
+          virtual ~TestClass() override;
+
+          virtual MxResult Tickle() override; // vtable+08
+
+          // FUNCTION: TEST 0x12345678
+          inline const char* ClassName() const // vtable+0c
+          {
+            // 0xabcd1234
+            return "TestClass";
+          }
+
+          // FUNCTION: TEST 0xdeadbeef
+          inline MxBool IsA(const char* name) const override // vtable+10
+          {
+            return !strcmp(name, TestClass::ClassName());
+          }
+
+        private:
+          int m_hello;
+          int m_hiThere;
+        };
+    """))
 
     assert len(parser.functions) == 2
     assert parser.functions[0].offset == int("0x12345678", 16)
-    assert parser.functions[0].line_number == 15
-    assert parser.functions[0].end_line == 19
+    assert parser.functions[0].line_number == 10
+    assert parser.functions[0].end_line == 14
 
     assert parser.functions[1].offset == int("0xdeadbeef", 16)
-    assert parser.functions[1].line_number == 22
-    assert parser.functions[1].end_line == 25
+    assert parser.functions[1].line_number == 17
+    assert parser.functions[1].end_line == 20
 
 
 def test_inline(parser):
-    with sample_file("inline.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // FUNCTION: TEST 0x10000001
+        inline const char* OneLineWithComment() const { return "MxDSObject"; }; // hi there
+
+        // FUNCTION: TEST 0x10000002
+        inline const char* OneLine() const { return "MxDSObject"; };
+    """))
 
     assert len(parser.functions) == 2
     for fun in parser.functions:
@@ -110,18 +203,38 @@ def test_multiple_offsets(parser):
     """If multiple offset marks appear before for a code block, take them
     all but ensure module name (case-insensitive) is distinct.
     Use first module occurrence in case of duplicates."""
-    with sample_file("multiple_offsets.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // FUNCTION: TEST 0x1234
+        // FUNCTION: HELLO 0x5555
+        void different_modules()
+        {
+          // TODO
+        }
+
+        // FUNCTION: TEST 0x2345
+        // FUNCTION: TEST 0x1234
+        void same_module()
+        {
+          // TODO
+        }
+
+        // FUNCTION: TEST 0x2002
+        // FUNCTION: test 0x1001
+        void same_case_insensitive()
+        {
+          // TODO
+        }
+    """))
 
     assert len(parser.functions) == 4
     assert parser.functions[0].module == "TEST"
-    assert parser.functions[0].line_number == 8
+    assert parser.functions[0].line_number == 3
 
     assert parser.functions[1].module == "HELLO"
-    assert parser.functions[1].line_number == 8
+    assert parser.functions[1].line_number == 3
 
     # Duplicate modules are ignored
-    assert parser.functions[2].line_number == 15
+    assert parser.functions[2].line_number == 10
     assert parser.functions[2].offset == 0x2345
 
     assert parser.functions[3].module == "TEST"
@@ -129,8 +242,17 @@ def test_multiple_offsets(parser):
 
 
 def test_variables(parser):
-    with sample_file("global_variables.cpp") as f:
-        parser.read(f.read())
+    parser.read(dedent("""\
+        // GLOBAL: TEST 0x1000
+        const char *g_message = "test";
+
+        // FUNCTION: TEST 0x1234
+        void function01()
+        {
+          // GLOBAL: TEST 0x5555
+          static int g_hello = 123;
+        }
+    """))
 
     assert len(parser.functions) == 1
     assert len(parser.variables) == 2

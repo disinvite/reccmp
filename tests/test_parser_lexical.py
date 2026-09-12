cab49ae051f40560a217ad425ade9e62103956e6
disinvite <disinvite@users.noreply.github.com>
2026-09-12 11:42:29 -0400

    Move tests around

--- tests/test_parser_lexical.py
+++ tests/test_parser_lexical.py
@@ -195,3 +195,73 @@ def test_struct_keyword_in_variable_type_is_not_scope(parser: DecompParser):
 
     # Did not qualify the variable name.
     assert "::" not in symbol.name
+
+
+# MarkerTypes for LINE annotations that can be qualified by a scope.
+# Function types are not here because the name we do not make an attempt
+# to isolate the function name from its line.
+QUALIFIED_TYPES = [
+    MarkerType.GLOBAL,
+    MarkerType.VTABLE,
+]
+
+
+@pytest.mark.parametrize("marker_type", QUALIFIED_TYPES)
+def test_namespace_multiline_comment(parser: DecompParser, marker_type: MarkerType):
+    """Should ignore misleading tokens when detecting the range for a named scope."""
+    scope_name = "Pizza"
+    token = completion_token(marker_type, AnnotationType.LINE)
+
+    # Make sure our scope is not part of the completion token already,
+    # because we are not checking the name.
+    assert scope_name not in token
+
+    parser.read(f"""\
+        namespace {scope_name} {{
+        /*
+        }}
+        */
+        // {marker_type.name}: TEST 0x1234
+        {token}
+        }}
+        """)
+
+    # Captured the marker.
+    (symbol,) = parser.iter_symbols()
+    assert symbol.type == marker_type
+
+    # Qualified the entity name with the outer scope.
+    assert scope_name in symbol.name
+
+    # No warnings.
+    assert not parser.alerts
+
+
+@pytest.mark.parametrize("marker_type", QUALIFIED_TYPES)
+def test_namespace_multiline_string(parser: DecompParser, marker_type: MarkerType):
+    """Should ignore misleading tokens when detecting the range for a named scope."""
+    scope_name = "Pizza"
+    token = completion_token(marker_type, AnnotationType.LINE)
+
+    # Make sure our scope is not part of the completion token already,
+    # because we are not checking the name.
+    assert scope_name not in token
+
+    parser.read(f"""\
+        namespace {scope_name} {{
+        const char *g_text = "}}\\
+        }}";
+        // {marker_type.name}: TEST 0x1234
+        {token}
+        }}
+        """)
+
+    # Captured the marker.
+    (symbol,) = parser.iter_symbols()
+    assert symbol.type == marker_type
+
+    # Qualified the entity name with the outer scope.
+    assert scope_name in symbol.name
+
+    # No warnings.
+    assert not parser.alerts
